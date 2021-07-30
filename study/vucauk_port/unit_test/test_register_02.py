b1f97b9fc0bb5f9b4bddb1fe4035a343c86c093e
zt <2219213864@qq.com>
2021-07-30 09:17:50 +0800

    结合数据库，调试注册第一步接口

--- study/vucauk_port/unit_test/test_register_02.py
+++ study/vucauk_port/unit_test/test_register_02.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+import ast
 from common import constant, request, mysql
 from configer import conf_class
 from ddt import ddt, data
@@ -8,7 +9,7 @@ from common.excel_class_02 import DoExcel
 
 cases01 = DoExcel(constant.case_dir_admin, "saveuserapplicationinfo").read_test_data()
 cases02 = DoExcel(constant.case_dir_admin, "sendRegisterMail").read_test_data()
-cases03 = DoExcel(constant.case_dir_admin, "getworkorder").read_test_data()
+cases03 = DoExcel(constant.case_dir_admin, "saveInvestorTypeInfo").read_test_data()
 data01 = []
 
 
@@ -17,15 +18,13 @@ class TestRegister(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # cls.host = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_host")
-        # cls.username = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_username")
-        # cls.password = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_password")
-        # cls.database = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database")
-        # cls.port = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_port")
-        # cls.mysql = mysql.Mysql(cls.host, cls.username, cls.password, cls.database, cls.port)
-        # cls.cursor = cls.mysql.cursor()
-        # useremail = cls.cursor.execute("select max(user163email) from t_user_info")
-        pass
+        cls.request = request.Request("test")
+        cls.host = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_host")
+        cls.username = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_username")
+        cls.password = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_password")
+        cls.database = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database")
+        cls.port = conf_class.Conf(constant.conffile_dir).getvalue("MysqlDatabase", "test_database_port")
+        cls.mysql = mysql.Mysql(cls.host, cls.username, cls.password, cls.database, int(cls.port))
 
     def setUp(self):
         pass
@@ -33,30 +32,42 @@ class TestRegister(unittest.TestCase):
     @data(*cases01)
     def test_case01_saveUserApplicationInfo(self, case):
 
-        # data = json.loads(case['data']
-        # if data['userapplyEmail'] == "${email}":
-        #     data['userapplyEmail'] =
-        response = self.request.request(case['method'], case['url'], json.loads(case['data']))
+        data = ast.literal_eval(case['data'])
+        if data['userapplyEmail'] == "${email}":
+            useremail = self.mysql.fetchone("select user163email FROM t_user_info ORDER BY id DESC LIMIT 1")
+            data['userapplyEmail'] = useremail[0]+"1"
+
+        response = self.request.request(case['method'], case['url'], data)
         try:
             self.assertIn(case['excepted'], response.json()['message'])
         except AssertionError as e:
-            DoExcel(constant.case_dir_admin, "getnotice").write_test_data(case["id"], response.text, "Failed")
+            DoExcel(constant.case_dir_admin, "saveuserapplicationinfo").write_test_data(case["id"], response.text, "Failed")
             print(e)
         else:
-            DoExcel(constant.case_dir_admin, "getnotice").write_test_data(case["id"], response.text, "Pass")
+            DoExcel(constant.case_dir_admin, "saveuserapplicationinfo").write_test_data(case["id"], response.text, "Pass")
 
     @data(*cases02)
     def test_case02_sendRegisterMail(self, case):
         # uid = data01[0]["results"]["uid"]
-        response = self.systemwork.post_sendRegisterMail(json.loads(case[4])["userId"],
-                                                         json.loads(case[4])["url"])
-        self.assertIn(json.loads(case[5]), response.text)
+        response = self.request.request(case['method'], case['url'], json.loads(case['data']))
+        try:
+            self.assertIn(case['excepted'], response.json()['message'])
+        except AssertionError as e:
+            DoExcel(constant.case_dir_admin, "sendRegisterMail").write_test_data(case["id"], response.text, "Failed")
+            print(e)
+        else:
+            DoExcel(constant.case_dir_admin, "sendRegisterMail").write_test_data(case["id"], response.text, "Pass")
 
-    @data(*cases03)
-    def test_case03_saveInvestorTypeInfo(self, case):
-        response = self.systemwork.post_saveInvestorTypeInfo(json.loads(case[4])["userId"],
-                                                             json.loads(case[4])["investorType"])
-        self.assertIn(json.loads(case[5]), response.text)
+    # @data(*cases03)
+    # def test_case03_saveInvestorTypeInfo(self, case):
+    #     response = self.request.request(case['method'], case['url'], json.loads(case['data']))
+    #     try:
+    #         self.assertIn(case['excepted'], response.json()['message'])
+    #     except AssertionError as e:
+    #         DoExcel(constant.case_dir_admin, "saveInvestorTypeInfo").write_test_data(case["id"], response.text, "Failed")
+    #         print(e)
+    #     else:
+    #         DoExcel(constant.case_dir_admin, "saveInvestorTypeInfo").write_test_data(case["id"], response.text, "Pass")
 
     def tearDown(self):
         pass
